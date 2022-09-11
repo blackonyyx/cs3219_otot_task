bd8fbf0bab37e79b8872fcc92a9bb2830dc1d12e
blackonyyx <35429757+blackonyyx@users.noreply.github.com>
2022-09-11 22:17:57 +0800

    Add connection to remote mongodb

--- api/model/db.js
+++ api/model/db.js
@@ -1,22 +1,22 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
-export const db = mongoose.connection;
+export const db = mongoose.connection
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.LOCAL_MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       // must add in order to not get any error masseges:
       useUnifiedTopology: true,
       useNewUrlParser: true,
-    });
-    console.log(`mongo database is connected!!! ${conn.connection.host} `);
+    })
+    console.log(`mongo database is connected!!! ${conn.connection.host} `)
   } catch (error) {
-    console.error(`Error: ${error} `);
-    process.exit(1); // passing 1 - will exit the proccess with error
+    console.error(`Error: ${error} `)
+    process.exit(1) // passing 1 - will exit the proccess with error
   }
-};
+}
 
-export default connectDB;
+export default connectDB
 
 // Models
 // require('./categoryModel')
