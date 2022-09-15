e420b3827e6db81526c533b3406ed210616f3cb7
Stephen Tan <35429757+blackonyyx@users.noreply.github.com>
2022-09-15 18:35:36 +0800

    all unique

--- backend/test/contributorTests.js
+++ backend/test/contributorTests.js
@@ -12,7 +12,7 @@ chai.should()
 
 const contributorPass = {
   name: "John",
-  email: "john@gmail.com",
+  email: "john6356@gmail.com",
   gender: "male",
   phone: "88888888",
   userDescription: "Lorum Ipsum",
